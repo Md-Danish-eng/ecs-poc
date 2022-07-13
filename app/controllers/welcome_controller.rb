1509e6d6e5ef79e2a23ab89941e53b60150bf978
Md-Danish-eng <85988020+Md-Danish-eng@users.noreply.github.com>
2022-07-13 09:41:47 +0530

    Update welcome_controller.rb

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def show
-    render text: "congratulations! Md Danish. you successfully deploy ROR app on Elastic container service."
+    render text: "congratulations! Md Danish. you successfully deploy ROR app on Elastic container service, testing"
   end
 end
